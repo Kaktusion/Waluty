aa4a00ab944bec96e16ad97c9e8649055c8c5e5d
KleminsK <KleminsK@PLMSNB0447>
2024-05-31 14:42:35 +0200

    Dodaj pliki projektów.

---
+++ Components/Logic/ExchangeRates.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using static System.Net.WebRequestMethods;
+
+namespace Logic
+{
+
+    public class ExchangeRatesTable
+    {
+
+        public string Table { get; set; }
+        public string No { get; set; }
+        public DateOnly EffectiveDate { get; set; }
+        public List<ExchangeRate> Rates { get; set; }
+
+        
+        
+    }
+    public class ExchangeRate
+    {
+        public string Currency { get; set; }
+        public string Code { get; set; }
+        public decimal Mid { get; set; }
+    }
+    public class Transaction
+    {
+        public DateTime Date { get; set; }
+        public string CurrencyCodeFrom { get; set; }
+        public string CurrencyCodeTo { get; set; }
+        public decimal MoneyIn { get; set; }
+        public decimal MoneyOut { get;set; }
+
+        public Transaction(DateTime date, string currencyCodeFrom, string currencyCodeTo, decimal moneyIn, decimal moneyOut)
+        {
+            Date = date;
+            CurrencyCodeFrom = currencyCodeFrom;
+            CurrencyCodeTo = currencyCodeTo;
+            MoneyIn = moneyIn;
+            MoneyOut = moneyOut;
+        }
+
+    }
+}
+
+    
+
+
+
